0c27313d65fb3d95e9497d88825461d2964cfd80
riteshgupta92 <riteshgupta.shyamagro@gmail.com>
2024-09-04 16:39:56 +0530

    Add cors

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,23 @@
-const express = require("express")
-const dotenv = require("dotenv").config()
-const connection = require("./db")
-const router = require("./routes/user")
-const cors = require("cors")
+const express = require("express");
+const dotenv = require("dotenv").config();
+const connection = require("./db");
+const router = require("./routes/user");
+const cors = require("cors");
 
-connection()
+connection();
 // middleware:-
-const app = express()
-app.use(express.json())
-app.use(cors())
-app.use("/api", router)
-
-
+const app = express();
+app.use(express.json());
+// Enable CORS for all routes
+app.use(
+  cors({
+    origin: "https://entertanment-app.onrender.com",
+  })
+);
+app.use("/api", router);
 
 // Running the server on localhost:4000
-const Port = process.env.PORT
-app.listen(Port, ()=>{
-    console.log(`Server is listening on ${Port}`)
-})
+const Port = process.env.PORT;
+app.listen(Port, () => {
+  console.log(`Server is listening on ${Port}`);
+});
