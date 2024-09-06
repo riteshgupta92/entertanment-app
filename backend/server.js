de2a808d768bffb677f3f8026b607141f0071974
riteshgupta92 <riteshgupta.shyamagro@gmail.com>
2024-09-06 21:24:44 +0530

    modify cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,9 @@ connection()
 // middleware:-
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin : 'https://entertanment-app.onrender.com'
+}))
 app.use("/api", router)
 
 // setup swagger:-
